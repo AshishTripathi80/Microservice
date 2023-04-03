83e815e41942ce3126c9346c4681df36993a6347
Ashish Tripathi <tripashi6@gmail.com>
2023-04-03 16:59:31 +0530

    01. init 3 projects

---
+++ movie-catlog-service/src/main/java/com/moviecatlogservice/MovieCatlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.moviecatlogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // this annotation tell that this is a spring boot project
+public class MovieCatlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatlogServiceApplication.class, args);
+	}
+
+}
